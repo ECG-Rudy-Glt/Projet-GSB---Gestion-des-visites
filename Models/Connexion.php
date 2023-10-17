d4823278d0b8147ca1d9f6e5f43783e881d35fa1
Floris91 <floris.sejourne@gmail.com>
2023-10-17 09:14:56 +0200

    gay
    gay

--- Models/Connexion.php
+++ Models/Connexion.php
@@ -5,7 +5,7 @@
 
 try {
     $host = 'localhost'; // Adresse du serveur MySQL
-    $dbname = 'GSB'; // Nom de la base de données GSB
+    $dbname = 'gsb'; // Nom de la base de données GSB
     $user = 'root'; // Nom d'utilisateur MySQL
 
     // Crée une nouvelle instance PDO
@@ -15,7 +15,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Le reste de votre code pour interagir avec la base de données va ici
-
+    echo "GG EZ";
 } catch (PDOException $e) {
     echo "Erreur de connexion à la base de données : " . $e->getMessage();
 }
