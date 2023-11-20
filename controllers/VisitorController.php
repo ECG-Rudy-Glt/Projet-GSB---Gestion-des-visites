b1662cff80440f994c1ef6267bdee89670436a7b
Rudy_glt <gault.rudy@gmail.com>
2023-11-20 17:30:09 +0100

    feat: you can now register
    when you register you are redirected to login to log

--- controllers/VisitorController.php
+++ controllers/VisitorController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @school ENSITECH
  * @company H2V Solutions
@@ -7,4 +6,57 @@
  * @updated_by Rudy GAULT
  * @updated_at 2023-11-13 16:18:48
  */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+require_once '../config.php';
+require_once '../models/VisiteurModel.php';
+
+class VisiteurController {
+    private $model;
+
+    public function __construct() {
+        global $dbConnection; // Assurez-vous d'avoir accès à cette variable
+        $this->model = new VisiteurModel($dbConnection);
+    }
+
+    public function register() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Afficher les données du formulaire
+            echo "<pre>";
+            var_dump($_POST);
+            echo "</pre>";
+
+            // Récupération des données du formulaire
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $name = $_POST['name'];
+            $surname = $_POST['surname'];
+            $tel = $_POST['tel'];
+
+            // Récupération des détails de l'adresse
+            $rue = $_POST['rue'];
+            $codepostal = $_POST['codepostal'];
+            $ville = $_POST['ville'];
+            $dpartement = $_POST['dpartement'];
+            $pays = $_POST['pays'];
+
+            // Effectuer l'inscription
+            $registrationResult = $this->model->register($name, $surname, $tel, $rue, $codepostal, $ville, $dpartement, $pays, $password);
+            var_dump($registrationResult); // Vérifie le résultat de l'inscription
+
+            if ($registrationResult) {
+                var_dump("Redirection en cours"); // Vérifie si le code atteint ce point
+                header("Location: ../views/login.html");
+                exit;
+            } else {
+                echo "Erreur lors de l'inscription";
+            }
+        }
+    }
+}
+
+$controller = new VisiteurController();
+$controller->register();
+?>
--- models/VisiteurModel.php
+++ models/VisiteurModel.php
@@ -8,3 +8,56 @@
  * @updated_at 2023-11-13 16:36:22
  */
 
+class VisiteurModel {
+    private $db;
+
+    public function __construct($dbConnection) {
+        $this->db = $dbConnection;
+    }
+
+    public function createAddress($rue, $codepostal, $ville, $dpartement, $pays) {
+        try {
+            $stmt = $this->db->prepare("INSERT INTO adresse (rue, codepostal, ville, dpartement, pays) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute([$rue, $codepostal, $ville, $dpartement, $pays]);
+            return $this->db->lastInsertId();
+        } catch (PDOException $e) {
+            // Afficher l'erreur SQL
+            echo "Erreur lors de la création de l'adresse: " . $e->getMessage();
+            $this->db->rollBack();
+            return false;
+        }
+    }
+
+    public function register($name, $surname, $tel, $rue, $codepostal, $ville, $dpartement, $pays, $password) {
+        try {
+            $this->db->beginTransaction();
+
+            // Créer l'adresse
+            $idadresse = $this->createAddress($rue, $codepostal, $ville, $dpartement, $pays);
+            if ($idadresse === false) {
+                throw new Exception("Erreur lors de la création de l'adresse.");
+            }
+
+            // Créer la personne
+            $stmtPersonne = $this->db->prepare("INSERT INTO personne (name, surname, tel, idadresse) VALUES (?, ?, ?, ?)");
+            if (!$stmtPersonne->execute([$name, $surname, $tel, $idadresse])) {
+                throw new Exception("Erreur lors de la création de la personne.");
+            }
+            $idpersonne = $this->db->lastInsertId();
+
+            // Créer le visiteur
+            $stmtVisiteur = $this->db->prepare("INSERT INTO visiteur (idpersonne, password, sel) VALUES (?, ?, ?)");
+            if (!$stmtVisiteur->execute([$idpersonne, $password, 'unSelUnique'])) {
+                throw new Exception("Erreur lors de la création du visiteur.");
+            }
+
+            $this->db->commit();
+            return true;
+        } catch (Exception $e) {
+            echo "Erreur lors de l'inscription: " . $e->getMessage();
+            $this->db->rollBack();
+            return false;
+        }
+    }
+}
+
